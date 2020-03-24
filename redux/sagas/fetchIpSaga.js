134dd5cf4cf3393f44518ee52c48e252cfc299b7
Jay <4a490063@stust.edu.tw>
2020-03-24 23:01:48 +0800

    [add] use take fork and select

--- redux/sagas/fetchIpSaga.js
+++ redux/sagas/fetchIpSaga.js
@@ -1,14 +1,23 @@
-import { put, takeEvery, call } from 'redux-saga/effects';
+import { put, call, take, fork, select } from 'redux-saga/effects';
 import { FETCH_IP, FETCH_IP_SUCCESS } from '../actions/fetchIpAction';
+import { FETCH_DATA_SUCCESS } from '../actions/fetchDataAction';
 
-function* fetchIp() {
-  const data = yield call(
+export const getData = state => state.fetchData;
+
+function* fetchIp(res) {
+  const { data } = yield select(getData);
+  console.log(res === data);
+  const ip = yield call(
     () => fetch('https://httpbin.org/ip')
       .then(response => response.json()),
   );
-  yield put({ type: FETCH_IP_SUCCESS, payload: { data } });
+  yield put({ type: FETCH_IP_SUCCESS, payload: { data: ip } });
 }
 
 export default function* fetchIpSaga() {
-  yield takeEvery(FETCH_IP, fetchIp);
+  while (true) {
+    const {payload: { data }} = yield take(FETCH_DATA_SUCCESS);
+    // yield fork(FETCH_IP, fetchIp);
+    yield fork(fetchIp, data);
+  }
 }
